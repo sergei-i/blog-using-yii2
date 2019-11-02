0f1f459ea4b8fddbf1e61df036b4012e1b571d1a
Sergei <1161441@gmail.com>
2019-11-02 15:37:15 +0200

    Added user authorization and registration

--- models/User.php
+++ models/User.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use Yii;
+use yii\web\IdentityInterface;
 
 /**
  * This is the model class for table "user".
@@ -16,7 +17,7 @@ use Yii;
  *
  * @property Comment[] $comments
  */
-class User extends \yii\db\ActiveRecord
+class User extends \yii\db\ActiveRecord implements IdentityInterface
 {
     /**
      * {@inheritdoc}
@@ -58,5 +59,55 @@ class User extends \yii\db\ActiveRecord
     public function getComments()
     {
         return $this->hasMany(Comment::className(), ['user_id' => 'id']);
+    }
+
+    public static function findIdentity($id)
+    {
+        return User::findOne($id);
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        // TODO: Implement findIdentityByAccessToken() method.
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getAuthKey()
+    {
+        // TODO: Implement getAuthKey() method.
+    }
+
+    public function validateAuthKey($authKey)
+    {
+        // TODO: Implement validateAuthKey() method.
+    }
+
+    public static function findByEmail($email)
+    {
+        return User::find()->where(['email' => $email])->one();
+    }
+
+    public function validatePassword($password)
+    {
+        return $this->password == $this->passwordVerify($password);
+    }
+
+    public function create()
+    {
+        return $this->save(false);
+    }
+
+    public function generatePassword($password)
+    {
+        return password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    public function passwordVerify($password)
+    {
+        return password_verify($password, $this->password);
     }
 }
